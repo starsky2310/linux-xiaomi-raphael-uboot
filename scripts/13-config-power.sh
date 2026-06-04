bef414154aa17e97e7a01f9bd58174b3d44560d1
GengWei1997 <gw19970326@Gmail.com>
2026-06-04 11:02:55 +0800

    Disable the WiFi power-saving mode

--- scripts/13-config-power.sh
+++ scripts/13-config-power.sh
@@ -37,4 +37,17 @@ EOF
 chroot rootdir systemctl enable blank_screen.service
 
 
+# 禁用 WiFi 省电模式，解决连接 WiFi 跳 ping 问题
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] [13]   └─ 禁用 WiFi 省电模式"
+mkdir -p rootdir/etc/NetworkManager/conf.d
+cat > rootdir/etc/NetworkManager/conf.d/wifi-powersave.conf << 'EOF'
+[connection]
+wifi.powersave = 2
+EOF
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] [13]   └─ 配置 ath10k 无线参数"
+mkdir -p rootdir/etc/modprobe.d
+cat > rootdir/etc/modprobe.d/ath10k.conf << 'EOF'
+options ath10k_core skip_otp=y
+EOF
+
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] [13] ✅ 电源管理配置完成"
